2da02e2cf1128999e91d7d5562edc4750d98d41b
Megha Bhargava <megha@cronj.com>
2023-02-11 19:28:45 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from "react";
 import "./App.scss";
 import Pagetitle from "./Atoms/PageTitle/PageTitle";
 import RouterClass from "./Routes/routes";
-import { ReactNotifications } from "react-notifications-component";
 
 const App = (props) => {
   return (
     <React.Fragment>
       <Pagetitle />
-      <ReactNotifications />
+
       <div className='App'>
         <RouterClass />
       </div>
