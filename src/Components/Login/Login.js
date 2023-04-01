0b0bc0d1979a1c7061f3bc367fb9fcdb5c16e548
Megha Bhargava <avengers.44team@gmail.com>
2023-03-31 23:54:00 -0400

    login chnages

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, TextBox, PageTitle } from "../../Atoms";
+import { Button, TextBox, PageTitle, Modal } from "../../Atoms";
 import "./style.scss";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
@@ -15,6 +15,7 @@ class Login extends Component {
     passWord: "",
     showPassword: false,
     errors: {},
+    sendOtp: false,
   };
   componentDidMount() {
     localStorage.clear();
@@ -35,23 +36,31 @@ class Login extends Component {
     } else if (prev.login !== cur.login && cur.login && !cur.login.valid) {
       alert(cur?.login?.loginStatus || "Something went wrong");
     }
+    if (prev.sendOtp !== cur.sendOtp && cur?.sendOtp && cur?.isSendOtp) {
+      this.setState({ sendOtp: true });
+    }
   }
+  sendOtp = () => {
+    const { userName } = this.state;
+    let err = {};
+
+    let regex = /^[a-zA-Z0-9.]+@+[a-zA-Z0-9]+.+[A-z]/;
+    if (!regex.test(userName)) {
+      err.userName = "Please enter valid emailId";
+      this.setState({ errors: err });
+    } else {
+      this.setState({ errors: {} });
+      this.props.userActions.sendOtp({ toEmail: userName });
+    }
+  };
   login = () => {
     const { userName, passWord } = this.state;
-    let err = {};
+
     if (userName && passWord) {
       this.setState({ errors: {} });
-      this.props.userActions.login({ username: userName, password: passWord });
-    } else if (this.state.userName === "" && this.state.passWord === "") {
-      err.userName = "UserName is Missing";
-      err.passWord = "password is Missing";
-      this.setState({ errors: err });
-    } else if (this.state.userName === "") {
-      err.userName = "UserName is Missing";
-      this.setState({ errors: err });
-    } else if (this.state.passWord === "") {
-      err.passWord = "password is Missing";
-      this.setState({ errors: err });
+      this.props.userActions.login({ email: userName, password: passWord });
+    } else if (passWord === null || passWord === "" || passWord === undefined) {
+      alert("Enter password recieved in email");
     }
   };
   render() {
@@ -69,13 +78,13 @@ class Login extends Component {
           <div className='broder'>
             <TextBox
               required
-              title={"UserName"}
+              title={"Email"}
               value={this.state.userName}
               onChange={(e) => this.setState({ userName: e.target.value })}
               error={this.state.errors?.userName}
               helperText={this.state.errors?.userName}
             />
-            <TextBox
+            {/* <TextBox
               required
               type={this.state.showPassword ? "text" : "password"}
               title={"password"}
@@ -111,10 +120,55 @@ class Login extends Component {
               onClick={() => (window.location.href = "/forgot-passsword")}
             >
               Forgot Password
-            </div>
-            <Button onClick={this.login} text={"Login"} />
+            </div> */}
+            <Button onClick={this.sendOtp} text={"Send Otp"} />
           </div>
         </div>
+        {this.state.sendOtp && (
+          <Modal
+            open={this.state.sendOtp}
+            handleClose={() =>
+              this.setState({
+                sendOtp: false,
+              })
+            }
+            close
+            className='sendOtpModal'
+          >
+            <TextBox
+              required
+              type={this.state.showPassword ? "text" : "password"}
+              title={"password"}
+              value={this.state.passWord}
+              onChange={(e) => this.setState({ passWord: e.target.value })}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position='end'>
+                    <IconButton
+                      aria-label='toggle password visibility'
+                      onClick={() =>
+                        this.setState({
+                          showPassword: !this.state.showPassword,
+                        })
+                      }
+                      style={{ marginTop: "0px" }}
+                      edge='start'
+                    >
+                      {this.state.showPassword ? (
+                        <VisibilityOff />
+                      ) : (
+                        <Visibility />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+              error={this.state.errors?.passWord}
+              helperText={this.state.errors?.passWord}
+            />{" "}
+            <Button onClick={this.login} text={"Send Otp"} />
+          </Modal>
+        )}
       </>
     );
   }
