2ef14b93031181c5b21b3f0217014f14b63eeef2
Team44Assistsu <avengers.44team@gmail.com>
2023-04-05 22:53:03 -0400

    changes for the comments

--- src/Atoms/Modal/Modal.js
+++ src/Atoms/Modal/Modal.js
@@ -6,26 +6,23 @@ import CloseIcon from "@mui/icons-material/Close"; // Importing CloseIcon compon
 const ModalComp = (props) => {
   // Declaring a functional component named ModalComp with props as input
   return (
-    // Rendering Modal component
     <Modal
       open={props.open} // Setting open property to a boolean value received from parent component
       onClose={props.handleClose} // Setting onClose property to a function received from parent component
       aria-labelledby="modal-modal-title" // Setting aria-labelledby property
       aria-describedby="modal-modal-description" // Setting aria-describedby property
     >
-      <div /* Div that contains the content of the Modal*/
+      <div
         style={props.style} // Applying style received from parent component
         className={`model-content ${props.className}`} // Applying class name received from parent component
       >
         {props.close && ( // Checking if close property is true
           <div className="closeIcon" onClick={props.handleClose}>
             {" "}
-            // Div for CloseIcon component
-            <CloseIcon /> // CloseIcon component from MUI library
+            <CloseIcon />
           </div>
         )}
-        {props.content || props.children} // Rendering either props.content or
-        props.children passed from parent component
+        {props.content || props.children}
       </div>
     </Modal>
   );
